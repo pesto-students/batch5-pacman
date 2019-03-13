654c015db261067d407ff1730038da2bbe4c2723
shivangg <xshivangx@gmail.com>
2019-03-14 05:13:25 +0530

    refactor: Use reduce for entityApplier
    refactor: Correct wallgenerator variable names
    
    refactor: Convert App to arrow function
    
    refactor: Add  separate fns under wallGenerators

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 
 import PacmanGame from './components/PacmanGame/PacmanGame';
 
-function App() {
-  return (
-    <PacmanGame width={500} numberofCells={50} />
-  );
-}
+const App = () => (
+  <PacmanGame width={500} numberofCells={50} />
+);
 
 export default App;
