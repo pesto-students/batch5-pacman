0c1be0245d6fb9940ff921501b5b44e1c56138cc
shivangg <xshivangx@gmail.com>
2019-03-13 10:24:49 +0530

    feat: Draw Pacman game entities

--- client/src/components/PacmanGame/PacmanGame.jsx
+++ client/src/components/PacmanGame/PacmanGame.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import Konva from 'konva';
 // import { Stage, Layer, Text, Circle, Line, Rect } from 'react-konva';
 
-import { initGridState } from './utils';
+import { initGridState, getFoods, getGhosts, getPacman, getWalls, getEnergizers, entityToCode } from './gameCore';
 
 import { PacmanBoard } from './PacmanBoard';
 
@@ -15,17 +15,44 @@ class PacmanGame extends Component {
   }
 
   componentDidMount() {
+    const canvasWidth = this.props.width;
     const cellsInEachRow = this.props.numberofCells;
+    const gridSize = canvasWidth / cellsInEachRow;
+    let gridState = initGridState(cellsInEachRow, cellsInEachRow);
 
-    const gridState = initGridState(cellsInEachRow, cellsInEachRow);
+    const entityApplier = (entityLocations, entityCode) => {
+      for (const [x, y] of entityLocations) {
+        gridState[x][y] = entityCode;
+      }
+      // return gridState;
+    };
 
-    // state mutations
 
+    // apply entities duummy data
+    entityApplier(getFoods(), entityToCode('food'));
+    entityApplier(getPacman(), entityToCode('pacman'));
+    console.log((gridState[43][5]));
+    // console.log(gridState[43])
+    entityApplier(getWalls(cellsInEachRow), entityToCode('wall'));
+    entityApplier(getGhosts(), entityToCode('ghost'));
+    console.log((gridState[43][5]));
+    entityApplier(getEnergizers(), entityToCode('energizer'));
 
-    this.setState({ gridState });
+    this.setState({ 'gridState': gridState });
+    this.setState({ 'gridSize': gridSize });
+
+    console.log(getPacman());
+    console.log((gridState[43][5]));
+    console.log('food', (gridState[15][9]));
+
+    // console.log('food', getFoods());
+    console.log('pacman', getPacman());
+    // console.log(getWalls());
+    console.log('ghost', getGhosts());
+
   }
   render() {
-    return <PacmanBoard state={this.state.gridState} />
+    return <PacmanBoard gridSize={this.state.gridSize} gridState={this.state.gridState} />
   }
 }
 
