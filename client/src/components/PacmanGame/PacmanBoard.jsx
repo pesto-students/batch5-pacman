6f4e1168ba058d6c972e9ff11db1d8930901dc35
shivangg <xshivangx@gmail.com>
2019-03-13 10:24:49 +0530

    feat: Draw Pacman game entities

--- client/src/components/PacmanGame/PacmanBoard.jsx
+++ client/src/components/PacmanGame/PacmanBoard.jsx
@@ -1,19 +1,54 @@
 import React from 'react';
 // import Konva from 'konva';
-import { Stage, Layer, Circle, Rect } from 'react-konva';
+import { Stage, Layer, Circle } from 'react-konva';
+import { codeToEntity } from './gameCore';
 
-function PacmanBoard(props) {
-  return <Stage width={window.innerWidth} height={window.innerHeight}>
+import Wall from './entities/Wall';
+import Ghost from './entities/Ghost';
+import Food from './entities/Food';
+import Pacman from './entities/Pacman';
+import Free from './entities/Free';
+
+function PacmanBoard({ gridState, gridSize }) {
+  if (gridState === '') {
+    return null;
+  }
+  console.log(gridSize);
+  console.log(codeToEntity(gridState[43][5]));
+  let game = [];
+  for (let row = 0; row < gridState.length; row++) {
+    const column = gridState[row];
+    for (let col = 0; col < column.length; col++) {
+      const entity = codeToEntity(gridState[row][col]);
+      switch (entity) {
+        case 'wall':
+          game.push(<Wall key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
+          break;
+        case 'food':
+          game.push(<Food key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
+          break;
+        case 'ghost':
+          game.push(<Ghost key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
+          break;
+        case 'pacman':
+          game.push(<Pacman key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
+          break;
+
+        default:
+          game.push(<Free key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
+          break;
+      }
+
+    }
+  }
+
+  // console.log(game);
+
+
+  return <Stage width={500} height={500}>
     <Layer>
-      <Rect
-        x={20}
-        y={50}
-        width={100}
-        height={100}
-        fill="red"
-        shadowBlur={10}
-      />
-      <Circle x={200} y={100} radius={50} fill="green" />
+      {game}
+      {/* <Pacman key={`${23 * 100}${12 * 100}`} x={43} y={5} gridSize={gridSize} /> */}
     </Layer>
   </Stage>
 }
