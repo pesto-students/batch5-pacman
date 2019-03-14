75d1b76d1a936c2c2430818e9ad157ece02c9d59
Mehul <Mehul>
2019-03-14 06:43:28 +0530

    Feat: Pacman movement & wall collision
    Fix: Rebase Fixes

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import PacmanGame from './components/PacmanGame/PacmanGame';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="game">
+        <PacmanGame width={500} numberofCells={50} />
+      </div>
+    );
+  }
+}
+
+export default App;
