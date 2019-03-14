6d4371b3b93a3aad5446f15fc42efa6e0cbd14f6
shivangg <xshivangx@gmail.com>
2019-03-14 09:07:14 +0530

    refactor: Delete App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 import PacmanGame from './components/PacmanGame/PacmanGame';
 
-class App extends Component {
-  render() {
-    return (
-      <PacmanGame width={500} numberofCells={50} />
-    );
-  }
-}
+const App = () => (
+  <PacmanGame width={500} numberofCells={50} />
+);
 
 export default App;
