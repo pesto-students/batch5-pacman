0b5065b5fbdd51e16df8507af8c5d06a697c683b
shivangg <xshivangx@gmail.com>
2019-03-14 05:40:39 +0530

    refactor: Use Object literal to make code DRY

--- client/src/components/PacmanGame/PacmanBoard.jsx
+++ client/src/components/PacmanGame/PacmanBoard.jsx
@@ -11,34 +11,27 @@ import Free from './entities/Free';
 import Energizer from './entities/Energizer';
 
 function PacmanBoard({ gridState, gridSize }) {
-  if (gridState === '') {
+  if (gridState === '' || gridSize === '') {
     return null;
   }
+
+  const entityComponent = {
+    wall: Wall,
+    food: Food,
+    ghost: Ghost,
+    pacman: Pacman,
+    energizer: Energizer,
+    free: Free,
+  };
+
   const game = [];
   for (let row = 0; row < gridState.length; row += 1) {
     const column = gridState[row];
     for (let col = 0; col < column.length; col += 1) {
       const entity = codeToEntity(gridState[row][col]);
-      switch (entity) {
-        case 'wall':
-          game.push(<Wall key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
-          break;
-        case 'food':
-          game.push(<Food key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
-          break;
-        case 'ghost':
-          game.push(<Ghost key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
-          break;
-        case 'pacman':
-          game.push(<Pacman key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
-          break;
-        case 'energizer':
-          game.push(<Energizer key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
-          break;
-        default:
-          game.push(<Free key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
-          break;
-      }
+      const EntityComponent = entityComponent[entity];
+
+      game.push(<EntityComponent key={`${row * 100}${col * 100}`} x={col} y={row} gridSize={gridSize} />);
     }
   }
 
