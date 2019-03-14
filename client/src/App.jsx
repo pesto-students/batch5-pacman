7615927b2781518d578927687c44dbed17e0520a
Mohd Hassaan <sr.hassan23@gmail.com>
2019-03-15 03:25:33 +0530

    feat: Add top navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import NavBar from './components/UI/NavBar';
 import PacmanGame from './components/PacmanGame/PacmanGame';
 
 const App = () => (
-  <PacmanGame width={500} numberofCells={50} />
+  <div>
+    <NavBar isLoggedIn />
+    <div className="container">
+      <PacmanGame width={500} numberofCells={50} />
+    </div>
+  </div>
 );
 
 export default App;
