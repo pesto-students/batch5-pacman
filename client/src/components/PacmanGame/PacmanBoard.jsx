2485126dd32867182fb75c8b4be65aed1acb4878
shivangg <xshivangx@gmail.com>
2019-03-14 11:57:09 +0530

    test: Add unit tests

--- client/src/components/PacmanGame/PacmanBoard.jsx
+++ client/src/components/PacmanGame/PacmanBoard.jsx
@@ -4,26 +4,24 @@ import { Stage, Layer } from 'react-konva';
 import { codeToEntity } from './gameCore';
 import Cell from './Cell';
 
-const PacmanBoard = ({ gridState, gridSize, pacman }) => (
+const PacmanBoard = ({ gridState, gridSize }) => (
   <Stage width={500} height={500}>
     <Layer>
       {
-        gridState.map((row, rowIndex) => {
-          return (
-            row.map((column, columnIndex) => {
-              const entity = codeToEntity(gridState[rowIndex][columnIndex]);
-              return (
-                <Cell
-                  key={`row-${rowIndex}-col-${columnIndex}`}
-                  y={columnIndex}
-                  x={rowIndex}
-                  gridSize={gridSize}
-                  entity={entity}
-                />
-              )
-            })
-          )
-        })
+        gridState.map((row, rowIndex) => (
+          row.map((column, columnIndex) => {
+            const entity = codeToEntity(gridState[rowIndex][columnIndex]);
+            return (
+              <Cell
+                key={`row-${rowIndex}-col-${columnIndex}`}
+                y={columnIndex}
+                x={rowIndex}
+                gridSize={gridSize}
+                entity={entity}
+              />
+            );
+          })
+        ))
       }
     </Layer>
   </Stage>
@@ -31,11 +29,10 @@ const PacmanBoard = ({ gridState, gridSize, pacman }) => (
 
 PacmanBoard.propTypes = {
   gridSize: PropTypes.number.isRequired,
-  pacman: PropTypes.arrayOf(
-    PropTypes.shape({
-      x: PropTypes.number.isRequired,
-      y: PropTypes.number.isRequired,
-    }),
+  gridState: PropTypes.arrayOf(
+    PropTypes.arrayOf(
+      PropTypes.number.isRequired,
+    ).isRequired,
   ).isRequired,
 
 };
