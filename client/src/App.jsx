57ab8978ce6bf6c5334697d778b4e7f8850d849f
Mohd Hassaan <sr.hassan23@gmail.com>
2019-03-18 07:39:33 +0530

    fix: Change navbar proptype

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-// import NavBar from './components/UI/NavBar';
+import NavBar from './components/UI/NavBar';
 import PacmanGame from './components/PacmanGame/PacmanGame';
 
 const App = () => (
   <div>
-    {/* <NavBar isLoggedIn /> */}
+    <NavBar isLoggedIn />
     <div className="container">
       <PacmanGame width={500} numberofCells={50} />
     </div>
