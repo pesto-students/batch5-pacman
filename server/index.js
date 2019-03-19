751cda4e6c96898905bcb5c873d77493bd8e6054
Kaushik Bokka <kaushikbokka@gmail.com>
2019-03-20 02:21:09 +0530

    chore: Add Socket setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const createError = require('http-errors');
+const socketio = require('socket.io');
+const http = require('http');
 require('dotenv/config');
 
 const app = express();
-
+const server = http.Server(app);
+const io = socketio(server);
 const config = require('./config/config');
 const routes = require('./api/api.js');
 const authRoutes = require('./api/auth.js');
-
 
 require('mongoose').connect(config.db.url, { useNewUrlParser: true });
 
@@ -19,6 +21,32 @@ app.use('/', authRoutes);
 
 app.use((req, res, next) => {
   next(createError(404));
+});
+
+const connections = [null, null];
+
+io.on('connection', (socket) => {
+  let playerIndex = -1;
+  connections.forEach((value, index) => {
+    if (value === null) {
+      playerIndex = index;
+    }
+  });
+  socket.emit('player-number', playerIndex);
+  if (playerIndex === -1) return;
+  connections[playerIndex] = socket;
+  socket.broadcast.emit('player-connect', playerIndex);
+  socket.on('actuate', (data) => {
+    const { pacman } = data;
+    const move = {
+      playerIndex,
+      pacman,
+    };
+    socket.on('disconnect', () => {
+      connections[playerIndex] = null;
+    });
+    socket.broadcast.emit('move', move);
+  });
 });
 
 // eslint-disable-next-line no-console
