47ae8d4175fc71097b47d3dc457ee37a03bb8bef
Shalini <shalinityagi64@gmail.com>
2019-03-20 22:37:08 +0530

    test: Setup test for client and add test for ScoreCard component

---
+++ client/src/components/UI/__tests__/scoreCard.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Table from '@material-ui/core/Table';
+import TableRow from '@material-ui/core/TableRow';
+import ScoreCard from '../ScoreCard';
+
+describe('Tests for <ScoreCard />', () => {
+  it('should render just one Table at a time', () => {
+    const wrapper = shallow(<ScoreCard />);
+    expect(wrapper.dive().find(Table).length).toBe(1);
+  });
+  it('should render three Rows if its detailed scorecard', () => {
+    const wrapper = shallow(<ScoreCard />);
+    expect(wrapper.dive().find(TableRow).length).toBe(3);
+  });
+  it('should render five Rows if its detailed scorecard', () => {
+    const wrapper = shallow(<ScoreCard isDetailed />);
+    expect(wrapper.dive().find(TableRow).length).toBe(5);
+  });
+});
