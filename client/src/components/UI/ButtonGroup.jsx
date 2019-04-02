425405134ec645ed9374a4575d13089f6be7eb2a
Saumya29 <saumyatiwari.29@gmail.com>
2019-04-02 07:11:07 +0530

    feat: Migrate start socket connect through modal

--- client/src/components/UI/ButtonGroup.jsx
+++ client/src/components/UI/ButtonGroup.jsx
@@ -4,7 +4,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import GoogleLogin from './GoogleLogin';
 
-
 const styles = theme => ({
   button: {
     margin: theme.spacing.unit,
@@ -17,19 +16,24 @@ class ButtonGroup extends React.Component {
 
   getButtonsToStartGame() {
     const {
-      classes,
-      history,
-      location,
-      userContext,
+      classes, history, location, userContext,
     } = this.props;
     const { displayPlayButton } = this.state;
     if (displayPlayButton || userContext.isLogIn) {
       return (
         <>
-          <Button variant="contained" className={classes.button} onClick={this.resetButtonsAndCloseModel}>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={this.resetButtonsAndLoadSinglePLayerPacman}
+          >
             Single Player
           </Button>
-          <Button variant="contained" className={classes.button} onClick={this.resetButtonsAndCloseModel}>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={this.resetButtonsAndLoadMultiplayerPacman}
+          >
             Multi Player
           </Button>
         </>
@@ -46,19 +50,23 @@ class ButtonGroup extends React.Component {
   }
 
   getButtonsEndGame = () => {
-    const {
-      classes,
-      history,
-      location,
-    } = this.props;
+    const { classes, history, location } = this.props;
     const { displayPlayButton } = this.state;
     if (displayPlayButton) {
       return (
         <>
-          <Button variant="contained" className={classes.button} onClick={this.resetButtonsAndCloseModel}>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={this.resetButtonsAndLoadSinglePLayerPacman}
+          >
             Single Player
           </Button>
-          <Button variant="contained" className={classes.button} onClick={this.resetButtonsAndCloseModel}>
+          <Button
+            variant="contained"
+            className={classes.button}
+            onClick={this.resetButtonsAndLoadMultiplayerPacman}
+          >
             Multi Player
           </Button>
         </>
@@ -67,22 +75,34 @@ class ButtonGroup extends React.Component {
     return (
       <>
         <GoogleLogin history={history} location={location} />
-        <Button variant="contained" className={classes.button} onClick={this.setDisplayPlayButton}>
+        <Button
+          variant="contained"
+          className={classes.button}
+          onClick={this.setDisplayPlayButton}
+        >
           Replay
         </Button>
       </>
     );
-  }
+  };
 
-  resetButtonsAndCloseModel = () => {
-    const { closeModal } = this.props;
+  resetButtonsAndLoadMultiplayerPacman = () => {
+    const { userContext, toLoader } = this.props;
     this.setState({ displayPlayButton: false });
+    toLoader();
+    userContext.startMultiPlayerGame();
+  };
+
+  resetButtonsAndLoadSinglePLayerPacman = () => {
+    const { userContext, closeModal } = this.props;
+    this.setState({ displayPlayButton: false });
+    userContext.startSinglePlayerGame();
     closeModal();
   };
 
   setDisplayPlayButton = () => {
     this.setState({ displayPlayButton: true });
-  }
+  };
 
   render() {
     const { mode } = this.props;
@@ -94,13 +114,13 @@ class ButtonGroup extends React.Component {
   }
 }
 
-
 ButtonGroup.propTypes = {
   classes: PropTypes.shape().isRequired,
   history: PropTypes.shape().isRequired,
   location: PropTypes.shape().isRequired,
   userContext: PropTypes.shape().isRequired,
   closeModal: PropTypes.func.isRequired,
+  toLoader: PropTypes.func.isRequired,
   mode: PropTypes.string.isRequired,
 };
 
