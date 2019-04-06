2e4d63bdecd7b2f9123abbfce3a134e7906f820d
hassan23 <sr.hassan23@gmail.com>
2019-04-07 04:42:21 +0530

    feat: Save score in mlab db

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-unused-state */
 import React from 'react';
+import axios from 'axios';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import uuid from 'uuid';
 import App from './App';
@@ -10,24 +11,33 @@ import {
   foundBothPlayer,
 } from './api/socketService';
 
+const serverUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost:9000';
+
 class PacmanRouter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLogIn: false,
       isGameStarted: false,
+      isGameEnd: false,
+      score: null,
       username: '',
       playerId: uuid.v1(),
       login: this.login,
       logout: this.logout,
       startMultiPlayerGame: this.startMultiPlayerGame,
       startSinglePlayerGame: this.startSinglePlayerGame,
+      setScore: this.setScore,
+      setEndGame: this.setEndGame,
+      saveScore: this.saveScore,
     };
   }
 
-  login = user => this.setState({ username: user, isLogIn: true });
+  login = (user, score) => {
+    this.setState({ username: user, isLogIn: true, score });
+  };
 
-  logout = () => this.setState({ username: '', isLogIn: false });
+  logout = () => this.setState({ username: '', isLogIn: false, score: null });
 
   startMultiPlayerGame = () => {
     const { playerId } = this.state;
@@ -40,7 +50,32 @@ class PacmanRouter extends React.Component {
   };
 
   startSinglePlayerGame = () => {
-    this.setState({ isGameStarted: true });
+    this.setState({ isGameStarted: true, isGameEnd: false });
+  };
+
+  setScore = (newScore) => {
+    const { score, isLogIn } = this.state;
+    const updatedScore = Number(score) > Number(newScore) ? Number(score) : Number(newScore);
+    this.setState({
+      score: updatedScore,
+      isGameEnd: true,
+      isGameStarted: false,
+    });
+    if (isLogIn) this.saveScore(updatedScore);
+  };
+
+  setEndGame = () => {
+    this.setState({ isGameEnd: true });
+  }
+
+  saveScore = (score) => {
+    const { username } = this.state;
+    axios.post(`${serverUrl}/score`, {
+      username,
+      score,
+    })
+      .then(() => {})
+      .catch(() => {});
   };
 
   render() {
