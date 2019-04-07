8e90ae606e29b3156a86d31578b1ab786e2e263d
Shalini <shalinityagi64@gmail.com>
2019-04-07 23:33:19 +0530

    chore: Add AWS deployment script

---
+++ server/deploy.sh
@@ -0,0 +1,8 @@
+mkdir ~/.aws
+touch ~/.aws/config
+chmod 600 ~/.aws/config
+
+echo "[eb-cli]" > ~/.aws/config
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
+eb deploy $BEANSTALK_ENVIRONMENT --region $BEANSTALK_AWS_REGION
